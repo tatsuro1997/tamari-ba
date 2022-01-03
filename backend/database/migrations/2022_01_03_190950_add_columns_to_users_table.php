b92fd4634547b3e1ad7eb89baec7d99a7d2b07f2
tatsuro1997 <minagawa@dptr.jp>
2022-01-03 19:21:11 +0900

    AddColumnsUsersTable

---
+++ backend/database/migrations/2022_01_03_190950_add_columns_to_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnsToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('age');
+            $table->integer('gender');
+            $table->string('prefecture');
+            $table->string('bike_type');
+            $table->string('bike_name');
+            $table->string('engine_size');
+            $table->integer('years_of_experience');
+            $table->boolean('through');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
