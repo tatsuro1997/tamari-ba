569de55ce13ee139ea22c6f6fdbfa58d6922970d
tatsuro1997 <minagawa@dptr.jp>
2022-01-05 09:25:04 +0900

    Add columnBikeBrand

--- backend/database/migrations/2022_01_05_085901_create_bikes_table.php
+++ backend/database/migrations/2022_01_05_085901_create_bikes_table.php
@@ -15,6 +15,7 @@ class CreateBikesTable extends Migration
     {
         Schema::create('bikes', function (Blueprint $table) {
             $table->id();
+            $table->string('bike_brand');
             $table->string('bike_type');
             $table->string('bike_name');
             $table->integer('engine_size');
