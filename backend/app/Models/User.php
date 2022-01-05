ad1c7c72e5bf31b11969d5ec50dcbebca5722d9d
tatsuro1997 <minagawa@dptr.jp>
2022-01-05 21:38:03 +0900

    Fix User

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -43,7 +43,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function road(){
+    public function roads(){
         return hasMany(Road::class);
     }
 }
