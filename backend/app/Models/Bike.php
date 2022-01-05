0a9953b51365da815cc26bbd30245f9c958b3542
tatsuro1997 <minagawa@dptr.jp>
2022-01-05 21:48:39 +0900

    Fix Model

--- backend/app/Models/Bike.php
+++ backend/app/Models/Bike.php
@@ -4,12 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Bike extends Model
 {
     use HasFactory;
 
-    public function user()
+    protected $fillable = [
+        'bike_brand',
+        'bike_type',
+        'bike_name',
+        'engine_size',
+    ];
+
+    public function users()
     {
         return belongsToMany(User::class);
     }
--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Bike;
 
 class User extends Authenticatable
 {
@@ -42,7 +43,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function bike()
+    public function bikes()
     {
         return belongsToMany(Bike::class);
     }
