79719b47bfdb9bfcaba87be25f64082be74ffc25
tatsuro1997 <minagawa@dptr.jp>
2022-01-08 10:41:32 +0900

    fix typo

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use App\Models\Raed;
+use App\Models\Road;
 
 class User extends Authenticatable
 {
