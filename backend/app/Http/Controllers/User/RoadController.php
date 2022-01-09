ad853f8ffe8f80be10504b41ae2e6ae2d49fc5bb
tatsuro1997 <minagawa@dptr.jp>
2022-01-09 15:58:55 +0900

    Add create, store

--- backend/app/Http/Controllers/User/RoadController.php
+++ backend/app/Http/Controllers/User/RoadController.php
@@ -5,6 +5,12 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Road;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Throwable;
+use Illuminate\Support\Facades\Log;
+use App\Http\Requests\RoadRequest;
+
 
 class RoadController extends Controller
 {
@@ -18,12 +24,40 @@ class RoadController extends Controller
 
     public function create()
     {
+        // $user_id = User::findOrFail($id);
+
         return view('user.roads.create');
     }
 
 
-    public function store($request)
+    public function store(RoadRequest $request)
     {
+
+
+        // dd($road);
+
+        try {
+            DB::transaction(function () use ($request) {
+                Road::create([
+                    'title' => $request->title,
+                    'latitude' => $request->latitude,
+                    'longitude' => $request->longitude,
+                    'description' => $request->description,
+                    'user_id' => 1, // TODO:authからuser_idを取得する
+                    // 'image1' => $request->image1,
+                    // 'image2' => $request->image2,
+                    // 'image3' => $request->image3,
+                    // 'image4' => $request->image4,
+                ]);
+            }, 2);
+        } catch (Throwable $e) {
+            Log::error($e);
+            throw $e;
+        }
+
+        return redirect()
+            ->route('user.roads.index')
+            ->with(['message' => '道を登録しました。', 'status' => 'info']);
     }
 
 
@@ -37,18 +71,15 @@ class RoadController extends Controller
     {
 
         return view('user.roads.edit');
-
     }
 
 
     public function update()
     {
-
     }
 
 
     public function destroy()
     {
-
     }
 }
