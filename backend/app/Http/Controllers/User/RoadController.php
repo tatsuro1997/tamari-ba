445ca055f0b15a09c8f2e885415994fa02b0a37a
tatsuro1997 <minagawa@dptr.jp>
2022-01-12 08:48:43 +0900

    Remove transaction

--- backend/app/Http/Controllers/User/RoadController.php
+++ backend/app/Http/Controllers/User/RoadController.php
@@ -24,32 +24,20 @@ class RoadController extends Controller
 
     public function create()
     {
-        // $user_id = User::findOrFail($id);
-
         return view('user.roads.create');
     }
 
 
     public function store(RoadRequest $request)
     {
-
-
-        // dd($road);
-
         try {
-            DB::transaction(function () use ($request) {
-                Road::create([
-                    'title' => $request->title,
-                    'latitude' => $request->latitude,
-                    'longitude' => $request->longitude,
-                    'description' => $request->description,
-                    'user_id' => 1, // TODO:authからuser_idを取得する
-                    // 'image1' => $request->image1,
-                    // 'image2' => $request->image2,
-                    // 'image3' => $request->image3,
-                    // 'image4' => $request->image4,
-                ]);
-            }, 2);
+            Road::create([
+                'title' => $request->title,
+                'latitude' => $request->latitude,
+                'longitude' => $request->longitude,
+                'description' => $request->description,
+                'user_id' => 1, // TODO:authからuser_idを取得する
+            ]);
         } catch (Throwable $e) {
             Log::error($e);
             throw $e;
