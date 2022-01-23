9104a1e221d730bae1e78a5942970f4626afd69b
tatsuro1997 <minagawa@dptr.jp>
2022-01-23 17:39:58 +0900

    Add CancelFunction

--- backend/resources/views/components/comment-form.blade.php
+++ backend/resources/views/components/comment-form.blade.php
@@ -4,17 +4,17 @@
         <div class="border-b border-blue-500 my-2">
             <input value="{{ $road->id }}" type="hidden" name="road_id" />
             <input value="{{ Auth::id() }}" type="hidden" name="user_id" />
-            <input class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" placeholder="コメント入力 ..." type="text" name="comment" />
+            <input id="commentForm" class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" placeholder="コメント入力 ..." type="text" name="comment" />
         </div>
         <button type="submit" class="flex-shrink-0 bg-blue-500 hover:bg-blue-700 border-blue-500 hover:border-blue-700 text-sm border-4 text-white py-1 px-2 rounded" >
             コメント
         </button>
-        <button type="button" class="flex-shrink-0 border-transparent border-4 text-gray-500 hover:text-gray-800 text-sm py-1 px-2 rounded">
+        <button type="button" onclick="cancelComment(this)" class="flex-shrink-0 border-transparent border-4 text-gray-500 hover:text-gray-800 text-sm py-1 px-2 rounded">
             キャンセル
         </button>
     </form>
     @foreach ($road->roadComments as $comment)
-        <div class="flex">
+        <div class="flex mt-4">
             <div class="font-medium mr-4">{{$comment->user->name}}</div>
             <div>{{$comment->created_at->format('Y-m-d')}}</div>
             <form id="delete_comment_{{$comment->id}}" method="post" action="{{ route('user.road.comment.destroy', ['road' => $comment->road_id, 'comment' => $comment->id ]) }}">
@@ -27,7 +27,7 @@
                 @endcan
             </form>
         </div>
-        <div class="text-xl">{{$comment->comment}}</div>
+        <div class="text-xl my-1">{{$comment->comment}}</div>
     @endforeach
 </div>
 
@@ -38,5 +38,11 @@
     if (confirm('本当に削除しても良いですか？')){
       document.getElementById('delete_comment_'+ e.dataset.id).submit();
     }
+  }
+
+  function cancelComment(){
+    'use strict';
+    let inputComemnt = document.getElementById('commentForm');
+    inputComment.value = ""
   }
 </script>
