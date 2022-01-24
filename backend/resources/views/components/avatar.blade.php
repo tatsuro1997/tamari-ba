db2f27765d16655bfa4ed3a51c495a74dbef1fbb
tatsuro1997 <minagawa@dptr.jp>
2022-01-24 09:40:39 +0900

    Update avatar.blade.php

--- backend/resources/views/components/avatar.blade.php
+++ backend/resources/views/components/avatar.blade.php
@@ -1,6 +1,6 @@
 <div class="h-12 w-12 flex items-center justify-center">
     @if (empty($avatar))
-        <img src="{{ asset('storage/images/' . Auth::user()->avatar) }}" class="rounded-full object-cover h-12 w-12">
+        <img src="{{ asset('storage/images/' . Auth::user()->avatar) }}" class="rounded-full object-cover h-12 w-12 mt-4">
     @else
         @if ($avatar == 'default_user.jpg')
             <img src="{{ asset('images/default_user.jpg') }}" class="rounded-full object-cover h-12 w-12">
