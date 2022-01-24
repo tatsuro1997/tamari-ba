7f9fc837b08d01e8921eb592b104d4a8a9a3d7d2
tatsuro1997 <minagawa@dptr.jp>
2022-01-24 09:36:03 +0900

    Add AvatarToCommentForm

--- backend/resources/views/components/avatar.blade.php
+++ backend/resources/views/components/avatar.blade.php
@@ -1,3 +1,11 @@
-<div class="h-16 w-16 flex items-center justify-center">
-    <img src="{{ asset('storage/images/' . Auth::user()->avatar) }}" class="rounded-full object-cover">
+<div class="h-12 w-12 flex items-center justify-center">
+    @if (empty($avatar))
+        <img src="{{ asset('storage/images/' . Auth::user()->avatar) }}" class="rounded-full object-cover h-12 w-12">
+    @else
+        @if ($avatar == 'default_user.jpg')
+            <img src="{{ asset('images/default_user.jpg') }}" class="rounded-full object-cover h-12 w-12">
+        @else
+            <img src="{{ asset('storage/images/' . $avatar) }}" class="rounded-full object-cover h-12 w-12">
+        @endif
+    @endif
 </div>
