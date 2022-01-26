dab960b1b301a32c4ac3d60ae6b57bd923462816
tatsuro1997 <minagawa@dptr.jp>
2022-01-26 09:41:34 +0900

    Update avatar.blade.php

--- backend/resources/views/components/avatar.blade.php
+++ backend/resources/views/components/avatar.blade.php
@@ -1,11 +1,22 @@
-<div class="h-12 w-12 flex items-center justify-center">
-    @if (empty($avatar))
-        <img src="{{ asset('storage/images/' . Auth::user()->avatar) }}" class="rounded-full object-cover h-12 w-12 mt-4">
-    @else
-        @if ($avatar == 'default_user.jpg')
-            <img src="{{ asset('images/default_user.jpg') }}" class="rounded-full object-cover h-12 w-12">
+@php
+    if($type === 'nav'){
+        $style = "rounded-full object-cover h-12 w-12 mt-2";
+    }
+    if($type === 'profile'){
+        $style = "rounded-full object-cover h-20 w-20";
+    }
+@endphp
+
+<div class="items-center justify-center">
+    <a href="{{ route('user.profile') }}">
+        @if (empty($avatar))
+            <img src="{{ asset('storage/images/' . Auth::user()->avatar) }}" class="{{$style}}">
         @else
-            <img src="{{ asset('storage/images/' . $avatar) }}" class="rounded-full object-cover h-12 w-12">
+            @if ($avatar == 'default_user.jpg')
+                <img src="{{ asset('images/default_user.jpg') }}" class="rounded-full object-cover h-12 w-12">
+            @else
+                <img src="{{ asset('storage/images/' . $avatar) }}" class="rounded-full object-cover h-12 w-12">
+            @endif
         @endif
-    @endif
+    </a>
 </div>
