99ddfec516344c57586fe9d4811b8e8288d8a93a
tatsuro1997 <minagawa@dptr.jp>
2022-01-28 09:29:09 +0900

    Update tag-form.blade.php

--- backend/resources/views/components/tag-form.blade.php
+++ backend/resources/views/components/tag-form.blade.php
@@ -1,22 +1,3 @@
-{{-- <div class="form-group row">
-    {!! Form::label('tags', 'タグ', ['class' => 'col-sm-2 control-label']) !!}
-    <div class="col-sm-10">
-        <div class="{{ $errors->has('tags.*') ? 'is-invalid' : '' }}">
-            @foreach ($tags as $key => $tag)
-                <div class="form-check form-check-inline">
-                    {!! Form::checkbox( 'tags[]', $key, null, ['class' => 'form-check-input', 'id' => 'tag'.$key]) !!}
-                    <label class="form-check-label" for="tag{{$key}}">{{ $tag }}</label>
-                </div>
-            @endforeach
-        </div>
-        @error('tags.*')
-            <span class="invalid-feedback" role="alert">
-                {{ $message }}
-            </span>
-        @enderror
-    </div>
-</div> --}}
-
 <div class="p-2 w-1/2 mx-auto">
     <div class="relative">
         <label for="image" class="leading-7 text-sm text-gray-600">タグ（複数可）</label>
