7e570ab2a36447ae6d8fe42052a83c8d071a3a21
tatsuro1997 <minagawa@dptr.jp>
2022-01-31 09:36:08 +0900

    Update list.blade.php

--- backend/resources/views/components/tag/list.blade.php
+++ backend/resources/views/components/tag/list.blade.php
@@ -1,6 +1,6 @@
 <ul class="flex flex-wrap">
     @foreach($tags as $tag)
-        <li class="mb-2 ml-2 text-sm shadow-sm bg-gray-200 rounded hover:text-white hover:bg-gray-500">
+        <li class="mb-2 ml-2 text-sm shadow-sm bg-gray-200 rounded">
             {{ '#' . $tag->name }}
         </li>
     @endforeach
