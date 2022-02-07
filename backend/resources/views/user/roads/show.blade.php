29e166c4eac98f342f11814567972a752eb11762
tatsuro1997 <minagawa@dptr.jp>
2022-02-07 09:26:37 +0900

    Road詳細の修正

--- backend/resources/views/user/roads/show.blade.php
+++ backend/resources/views/user/roads/show.blade.php
@@ -16,39 +16,42 @@
                                 <button onclick="location.href='{{ route('user.roads.index') }}'"  class="text-black bg-gray-200 border-0 py-2 px-8 focus:outline-none hover:bg-gray-400 rounded text-lg">一覧に戻る</button>
                             </div>
                             <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                                <div class="flex">
-                                    <x-swiper :images="$road->roadImages" type="road" />
-                                    <div class="w-1/2">
+                                <div class="lg:flex">
+                                    <x-swiper :images="$road->roadImages" type="road"/>
+                                    <div class="lg:w-1/2 sm:w-full">
                                         <div id="map" class="w-full h-80">
                                     </div>
                                 </div>
                             </div>
                             <div class="p-6 relative">
-                                <h1 class="title-font text-lg font-medium text-gray-900 mb-3">{{ $road->title }}</h1>
-                                <p class="leading-relaxed mb-3">{{ $road->description }}</p>
                                 <div class="flex justify-between">
+                                    <h1 class="title-font text-lg font-medium text-gray-900 mb-3">{{ $road->title }}</h1>
+                                    <x-like :road="$road" :like="$like" />
+                                </div>
+                                <div class="flex justify-between">
+                                    <p class="leading-relaxed mb-3">{{ $road->description }}</p>
                                     <div class="flex">
                                         @can('update', $road)
-                                            <button  onclick="location.href='{{ route('user.roads.edit', ['road' => $road->id ]) }}'" class="text-black bg-yellow-400 border-0 py-2 px-6 mr-2 focus:outline-none hover:bg-yellow-500 rounded">編集</button>
+                                            <a onclick="location.href='{{ route('user.roads.edit', ['road' => $road->id ]) }}'" class="py-2 px-2"><i class="far fa-edit"></i></a>
                                         @endcan
                                         @can('delete', $road)
                                             <form id="delete_{{ $road->id }}" method="post" action="{{ route('user.roads.destroy', ['road' => $road->id ]) }}">
                                                 @csrf
                                                 @method('delete')
                                                 <div class="w-full flex justify-around">
-                                                    <a href="#" data-id="{{ $road->id }}" onclick="deletePost(this)" class="text-white bg-red-400 border-0 py-4 px-6 focus:outline-none hover:bg-red-500 rounded">削除</a>
+                                                    <a href="#" data-id="{{ $road->id }}" onclick="deletePost(this)" class="border-0 py-2"><i class="far fa-trash-alt"></i></a>
                                                 </div>
                                             </form>
                                         @endcan
                                     </div>
-                                    <div>
-                                        <p class="leading-relaxed text-right">{{ $road->created_at->format('Y-m-d') }}</p>
-                                        <p class="leading-relaxed text-right">{{ $road->user->name }}</p>
-                                    </div>
+                                </div>
+                                <div class="justify-end">
+                                    <p class="leading-relaxed text-right">{{ $road->created_at->format('Y-m-d') }}</p>
+                                    <p class="leading-relaxed text-right">{{ $road->user->name }}</p>
                                 </div>
                             </div>
-                            <x-comment.form :original="$road" type="road" />
                         </div>
+                        <x-comment.form :original="$road" type="road" />
                     </section>
                 </div>
             </div>
