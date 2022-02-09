f8f80d144887e34d56ea9bdc98dfbe1be097810d
tatsuro1997 <minagawa@dptr.jp>
2022-02-10 05:40:31 +0900

    Bike編集機能の追加

--- backend/app/Http/Controllers/User/BikeController.php
+++ backend/app/Http/Controllers/User/BikeController.php
@@ -64,4 +64,52 @@ class BikeController extends Controller
             ->route('user.bikes.index')
             ->with(['message' => 'バイクを登録しました。', 'status' => 'info']);
     }
+
+
+    public function edit(Bike $bike)
+    {
+        $this->authorize('edit', $bike);
+
+        $bike = Bike::findOrFail($bike->id);
+        // $tags = Tag::pluck('name', 'id')->toArray();
+
+        return view('user.bikes.edit', compact('bike'));
+    }
+
+
+    public function update(BikeRequest $request, Bike $bike)
+    {
+        $this->authorize('update', $bike);
+
+        $bike = Bike::findOrFail($bike->id);
+        $bike->title = $request->title;
+        $bike->bike_brand = $request->bike_brand;
+        $bike->bike_type = $request->bike_type;
+        $bike->bike_name = $request->bike_name;
+        $bike->engine_size = $request->engine_size;
+        $bike->description = $request->description;
+        $bike->save();
+
+        // $bike->tags()->sync($request->tags);
+
+        $imageFiles = $request->file('files');
+
+        if ($imageFiles) {
+            foreach ($bike->bikeImages as $image) {
+                Storage::delete('public/bikes/' . $image->filename); //Storageから以前の画像を削除
+            }
+            BikeImage::where('bike_id', $bike->id)->delete(); //DBから以前の画像を削除
+            foreach ($imageFiles as $imageFile) {
+                $fileNameToStore = ImageService::upload($imageFile, 'bikes');
+                BikeImage::create([
+                    'bike_id' => $bike->id,
+                    'filename' => $fileNameToStore,
+                ]);
+            }
+        }
+
+        return redirect()
+            ->route('user.bikes.index')
+            ->with(['message' => 'バイクの投稿を更新しました。', 'status' => 'info']);
+    }
 }
