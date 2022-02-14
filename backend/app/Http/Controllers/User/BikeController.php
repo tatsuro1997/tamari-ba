4089644a3ed064d11caea9985d0b069942cf0f64
tatsuro1997 <minagawa@dptr.jp>
2022-02-14 09:40:44 +0900

    N+1対策

--- backend/app/Http/Controllers/User/BikeController.php
+++ backend/app/Http/Controllers/User/BikeController.php
@@ -21,7 +21,7 @@ class BikeController extends Controller
 {
     public function index(Request $request)
     {
-        $bikes = Bike::with('tags', 'bikeComments')->withCount('bikeLikes')->orderBy('created_at', 'desc')->paginate(12);
+        $bikes = Bike::with('tags', 'maker', 'type', 'bikeLikes', 'bikeImages', 'user')->withCount('bikeLikes')->orderBy('created_at', 'desc')->paginate(12);
 
         $like = new BikeLike;
 
