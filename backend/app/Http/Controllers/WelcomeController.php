1284ea05e3149c4cc5e9dc7ef929d744d52311d4
tatsuro1997 <minagawa@dptr.jp>
2022-02-21 16:05:37 +0900

    キャッシュ化できていないので削除

--- backend/app/Http/Controllers/WelcomeController.php
+++ backend/app/Http/Controllers/WelcomeController.php
@@ -11,10 +11,6 @@ class WelcomeController extends Controller
 {
     public function welcome()
     {
-        $value = Cache::remember('roads', 600, function () {
-            return DB::table('roads')->get();
-        });
-
         $roads = Road::with('tags', 'roadImages', 'user')->orderBy('created_at', 'desc')->take(3)->get();
 
         return view('user.welcome', compact('roads'));
