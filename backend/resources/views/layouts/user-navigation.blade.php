af371d51d6d15500d1d35294f184c8f88ef4c6fd
tatsuro1997 <minagawa@dptr.jp>
2022-03-10 14:45:17 +0900

    道の投稿をメインに変更

--- backend/resources/views/layouts/user-navigation.blade.php
+++ backend/resources/views/layouts/user-navigation.blade.php
@@ -21,13 +21,13 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('user.bikes.index')" :active="request()->routeIs('user.bikes.index')">
-                        {{ __('バイクの投稿') }}
+                    <x-nav-link :href="route('user.roads.index')" :active="request()->routeIs('user.roads.index')">
+                        {{ __('道の投稿') }}
                     </x-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('user.roads.index')" :active="request()->routeIs('user.roads.index')">
-                        {{ __('道の投稿') }}
+                    <x-nav-link :href="route('user.bikes.index')" :active="request()->routeIs('user.bikes.index')">
+                        {{ __('バイクの投稿') }}
                     </x-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
@@ -109,13 +109,13 @@
         <!-- Responsive Navigation Menu -->
         <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
             <div class="pt-2 pb-3 space-y-1">
-                <x-responsive-nav-link :href="route('user.bikes.index')" :active="request()->routeIs('user.bikes.index')">
-                    {{ __('バイクの投稿') }}
+                <x-responsive-nav-link :href="route('user.roads.index')" :active="request()->routeIs('user.roads.index')">
+                    {{ __('道の投稿') }}
                 </x-responsive-nav-link>
             </div>
             <div class="pt-2 pb-3 space-y-1">
-                <x-responsive-nav-link :href="route('user.roads.index')" :active="request()->routeIs('user.roads.index')">
-                    {{ __('道の投稿') }}
+                <x-responsive-nav-link :href="route('user.bikes.index')" :active="request()->routeIs('user.bikes.index')">
+                    {{ __('バイクの投稿') }}
                 </x-responsive-nav-link>
             </div>
             <div class="pt-2 pb-3 space-y-1">
