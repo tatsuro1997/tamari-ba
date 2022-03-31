f4994a97af7c1c49375345de454dbb84b636ad87
tatsuro1997 <minagawa@dptr.jp>
2022-03-31 18:44:52 +0900

    excelInsertBase

--- backend/config/app.php
+++ backend/config/app.php
@@ -177,6 +177,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         App\Providers\SampleServiceProvider::class,
         App\Providers\SlackServiceProvider::class,
+        Maatwebsite\Excel\ExcelServiceProvider::class,
 
     ],
 
@@ -234,6 +235,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'InterventionImage' => Intervention\Image\Facades\Image::class,
         'Slack' => App\Services\Slack\SlackFacade::class,
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
 
     ],
 
