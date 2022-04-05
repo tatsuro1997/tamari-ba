7429a87e6d1adfa768b99e6ffff34fc64cf66352
tatsuro1997 <minagawa@dptr.jp>
2022-04-06 06:59:44 +0900

    NavにIconを追加

--- backend/resources/v2/js/components/layout/MainNavigation.js
+++ backend/resources/v2/js/components/layout/MainNavigation.js
@@ -1,7 +1,12 @@
+import icon from '../../../../../public/images/icon.webp'
+
 const MainNavigation = () => {
     return (
-        <header className="w-full h-16 flex justify-between bg-white p-4">
-            <div className="w-1/4 text-2xl icon-color font-bold">Tamari-Ba</div>
+        <header className="w-full h-20 flex justify-between bg-white p-4">
+            <div className='flex w-1/4'>
+                <img loading="lazy" width="50" height="50" srcset={icon} alt="icon" className="lazyload h-14 sm:mr-4 sm:pb-2" />
+                <div className="text-2xl icon-color font-bold leading-10">Tamari-Ba</div>
+            </div>
             <nav className="w-3/4 flex justify-between leading-8">
                 <ul className="flex">
                     <li className="mr-4 nav-content">
