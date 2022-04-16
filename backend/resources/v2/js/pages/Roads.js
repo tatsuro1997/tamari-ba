a482f857b4ef7fec894e31c415b8a3d3a3136b7b
tatsuro1997 <minagawa@dptr.jp>
2022-04-17 07:11:55 +0900

    絞り込み機能の追加

--- backend/resources/v2/js/pages/Roads.js
+++ backend/resources/v2/js/pages/Roads.js
@@ -3,24 +3,48 @@ import React, { useState, useEffect, Fragment } from 'react';
 import RoadList from "../components/roads/RoadList";
 
 const Roads = () => {
-
     const [loadedRoads, setLoadedRoads] = useState([]);
+    const [searchKeyword, updateSearchKeyword] = useState('');
 
     useEffect(
         () => {
-            axios
-                .get('/api/roads')
-                .then((res) => {
-                    setLoadedRoads(res.data.data);
-                })
-                .catch((e) => {
-                    console.log(e);
-                })
+                axios
+                    .get('/api/roads')
+                    .then((res) => {
+                        setLoadedRoads(res.data.data);
+                    })
+                    .catch((e) => {
+                        console.log(e);
+                    })
         }, []);
+
+    const searchChangeHandler = event => {
+        updateSearchKeyword(event.target.value);
+        let value = event.target.value.toLowerCase();
+        let result = [];
+        console.log(value);
+
+        result = loadedRoads.filter((road) => {
+            return road.title.includes(value);
+        });
+        setLoadedRoads(result);
+    }
 
     return (
         <Fragment>
-            <RoadList roads={loadedRoads}/>
+            <div>
+                <label htmlFor="search-keyword">Search</label>
+                <input
+                    id="search-keyword"
+                    type="text"
+                    placeholder={"検索"}
+                    onChange={searchChangeHandler}
+                />
+            </div>
+            <RoadList
+                roads={loadedRoads}
+                searchKeyword={searchKeyword}
+            />
         </Fragment>
     )
 };
