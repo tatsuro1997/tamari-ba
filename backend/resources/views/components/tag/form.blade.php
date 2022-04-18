b3425498d48cba9ffef3707ab40b10e8620c13d6
tatsuro1997 <minagawa@dptr.jp>
2022-04-19 07:22:00 +0900

    タグを一時非表示に変更

--- backend/resources/views/components/tag/form.blade.php
+++ backend/resources/views/components/tag/form.blade.php
@@ -6,23 +6,25 @@
         $rTag = $road->tags;
    }
 @endphp
-<div class="p-2 w-1/2 mx-auto">
-    <div class="relative">
-        <label for="image" class="leading-7 text-sm text-gray-600">タグ（複数可）</label>
-        <div class="col-sm-10">
-            <div class="{{ $errors->has('tags.*') ? 'is-invalid' : '' }}">
-                @foreach ($tags as $key => $tag)
-                    <div class="form-check form-check-inline">
-                        <input id="tag{{$key}}" type="checkbox" name="tags[]" value="{{$key}}" @if(explode('.', Route::currentRouteName())[2]==='edit') @if($key===$rTag->first()->pivot->tag_id)checked @endif @endif>
-                        <label class="form-check-label" for="tag{{$key}}">{{ $tag }}</label>
-                    </div>
-                @endforeach
+@if (!isset($rTag))
+    <div class="p-2 w-1/2 mx-auto">
+        <div class="relative">
+            <label for="image" class="leading-7 text-sm text-gray-600">タグ（複数可）</label>
+            <div class="col-sm-10">
+                <div class="{{ $errors->has('tags.*') ? 'is-invalid' : '' }}">
+                    @foreach ($tags as $key => $tag)
+                        <div class="form-check form-check-inline">
+                            <input id="tag{{$key}}" type="checkbox" name="tags[]" value="{{$key}}" @if(explode('.', Route::currentRouteName())[2]==='edit') @if($key===$rTag->first()->pivot->tag_id)checked @endif @endif>
+                            <label class="form-check-label" for="tag{{$key}}">{{ $tag }}</label>
+                        </div>
+                    @endforeach
+                </div>
+                @error('tags.*')
+                    <span class="invalid-feedback" role="alert">
+                        {{ $message }}
+                    </span>
+                @enderror
             </div>
-            @error('tags.*')
-                <span class="invalid-feedback" role="alert">
-                    {{ $message }}
-                </span>
-            @enderror
         </div>
     </div>
-</div>
+@endif
