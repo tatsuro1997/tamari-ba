1f0cc20bf1ecf65e5f87e226d3eb059cb30d0bc3
tatsuro1997 <minagawa@dptr.jp>
2022-04-25 07:47:15 +0900

    Navigation, Menuの表示をレスポンシブ化

--- backend/resources/v2/js/components/layout/Menu.js
+++ backend/resources/v2/js/components/layout/Menu.js
@@ -1,8 +1,41 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { slide as Menu } from "react-burger-menu";
 
+import axios from 'axios';
+import swal from 'sweetalert';
+
 export default props => {
+    const navigate = useNavigate();
+
+    const logoutSubmit = (e) => {
+        e.preventDefault();
+
+        axios.post(`/api/logout`).then(res => {
+            if (res.data.status === 200) {
+                localStorage.removeItem('auth_token', res.data.token);
+                localStorage.removeItem('auth_name', res.data.username);
+                swal("ログアウトしました", res.data.message, "success");
+                navigate('/');
+                location.reload();
+            }
+        });
+    }
+
+    var AuthButtons = '';
+
+    if (!localStorage.getItem('auth_token')) {
+        AuthButtons = (
+            <Link to="/login" className="bm-item menu-item mb-10 nav-content">ログイン</Link>
+        );
+    } else {
+        AuthButtons = (
+            <li className="bm-item menu-item mb-10 nav-content">
+                <button onClick={logoutSubmit}>ログアウト</button>
+            </li>
+        );
+    }
+
     return (
         <Menu {...props}>
             <Link to="/v2/welcome" className="menu-item mb-10 nav-content" >
@@ -17,9 +50,18 @@ export default props => {
                 バイクの投稿
             </Link> */}
 
-            <Link to="/v2/boards" className="menu-item nav-content" >
+            <Link to="/v2/boards" className="menu-item mb-10 nav-content" >
                 コミュニティ
             </Link>
+
+            {AuthButtons}
+
+            <Link
+                to="/register"
+                className="inline-block font-normal mx-auto text-black bg-orange-400 border-0 focus:outline-none hover:bg-orange-500 rounded-full text-2xl h-12 leading-10 text-center">
+                Tamari-Baに参加
+            </Link>
+
         </Menu>
     );
 };
